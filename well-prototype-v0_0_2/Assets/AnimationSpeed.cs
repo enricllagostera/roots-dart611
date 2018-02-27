57f242010cbd8d0b8b8481bffff73ff1a2e15164
Rebecca <rebecgood@gmail.com>
2018-02-27 18:23:28 -0500

    Main grow cycle done
    Everything grows! What a season!
    Each animation is 4 frames, could simplify or lengthen if we need, but will keep it at that for now.

--- well-prototype-v0_0_2/Assets/AnimationSpeed.cs
+++ well-prototype-v0_0_2/Assets/AnimationSpeed.cs
@@ -8,6 +8,15 @@ public class AnimationSpeed : MonoBehaviour {
  public Animator animator3;
  public Animator animator4;
  public Animator animator5;
+ public Animator animator6;
+ public Animator animator7;
+ public Animator animator8;
+ public Animator animator9;
+ public Animator animator10;
+ public Animator animator11;
+ public Animator animator12;
+ public Animator animator13;
+ public Animator animator14;
 
  public float desiredSpeed;
 
@@ -25,6 +34,16 @@ public class AnimationSpeed : MonoBehaviour {
  	animator3.speed = desiredSpeed;
  	animator4.speed = desiredSpeed;
  	animator5.speed = desiredSpeed;
+ 	animator6.speed = desiredSpeed;
+ 	animator7.speed = desiredSpeed;
+ 	animator8.speed = desiredSpeed;
+ 	animator9.speed = desiredSpeed;
+ 	animator10.speed = desiredSpeed;
+ 	animator11.speed = desiredSpeed;
+ 	animator12.speed = desiredSpeed;
+ 	animator13.speed = desiredSpeed;
+ 	animator14.speed = desiredSpeed;
+
 		
 	}
 }
