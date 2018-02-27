4a8f83ac11a8ddb351d1630681d76acf1d873a49
Rebecca <rebecgood@gmail.com>
2018-02-27 00:03:09 -0500

    Animations Push
    In progress animations have been added to the Anima2D example file for now!

---
+++ well-prototype-v0_0_2/Assets/AnimationSpeed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeed : MonoBehaviour {
+ public Animator animator1;
+ public Animator animator2;
+ public Animator animator3;
+ public Animator animator4;
+ public Animator animator5;
+
+ public float desiredSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+ 
+ 	animator1.speed = desiredSpeed;
+ 	animator2.speed = desiredSpeed;
+ 	animator3.speed = desiredSpeed;
+ 	animator4.speed = desiredSpeed;
+ 	animator5.speed = desiredSpeed;
+		
+	}
+}
