0012f92c3e80ab1639613e1abd462213475f9c72
Enric Llagostera <enricllagostera@gmail.com>
2018-03-01 16:12:45 -0500

    project re-org

---
+++ well-prototype-v0_0_2/Assets/Roots/AnimationSpeed.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeed : MonoBehaviour {
+ public Animator animator1;
+ public Animator animator2;
+ public Animator animator3;
+ public Animator animator4;
+ public Animator animator5;
+ public Animator animator6;
+ public Animator animator7;
+ public Animator animator8;
+ public Animator animator9;
+ public Animator animator10;
+ public Animator animator11;
+ public Animator animator12;
+ public Animator animator13;
+ public Animator animator14;
+
+ public float desiredSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+ 
+ 	animator1.speed = desiredSpeed;
+ 	animator2.speed = desiredSpeed;
+ 	animator3.speed = desiredSpeed;
+ 	animator4.speed = desiredSpeed;
+ 	animator5.speed = desiredSpeed;
+ 	animator6.speed = desiredSpeed;
+ 	animator7.speed = desiredSpeed;
+ 	animator8.speed = desiredSpeed;
+ 	animator9.speed = desiredSpeed;
+ 	animator10.speed = desiredSpeed;
+ 	animator11.speed = desiredSpeed;
+ 	animator12.speed = desiredSpeed;
+ 	animator13.speed = desiredSpeed;
+ 	animator14.speed = desiredSpeed;
+
+		
+	}
+}
