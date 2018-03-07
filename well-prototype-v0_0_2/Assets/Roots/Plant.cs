2de560b1702c97fa8f30e82a4db496482f741377
Enric Llagostera <enricllagostera@gmail.com>
2018-03-07 08:45:24 -0500

    animation issues solved

--- well-prototype-v0_0_2/Assets/Roots/Plant.cs
+++ well-prototype-v0_0_2/Assets/Roots/Plant.cs
@@ -3,51 +3,65 @@ using System.Collections.Generic;
 
 public class Plant : MonoBehaviour
 {
+    public int plantIndex;
     // input
     public KeyCode nutrientKey;
     public KeyCode growthKey;
     public KeyCode reproduceKey;
     // gameplay
-    public float health; // 0..1
     public float growth; // 0..1
+    public float health; // 0..1
     public float growthSpeed;
-    public float healthDecay;
+    public float healthSpeed;
     public float growthDecay;
+    public float healthDecay;
+    public float wiltSpeed;
     //FX
     private ParticleFX nutrientFX;
     private ParticleFX reproduceFX;
     private SpriteRenderer _visual;
+    private Animator _animator;
+    public float baseSpeed;
+
+    public bool activeGrowth;
+    public bool activeNutrient;
 
     void Start()
     {
         // wire-up
-        nutrientFX = transform.Find("Nutrient").GetComponentInChildren<ParticleFX>();
-        reproduceFX = transform.Find("Reproduce").GetComponentInChildren<ParticleFX>();
+        //nutrientFX = transform.Find("Nutrient").GetComponentInChildren<ParticleFX>();
+        //reproduceFX = transform.Find("Reproduce").GetComponentInChildren<ParticleFX>();
         _visual = GetComponentInChildren<SpriteRenderer>();
+        _animator = GetComponentInChildren<Animator>();
         // gameplay init
-        health = 1f;
+        health = 0f;
         growth = 0f;
+        baseSpeed = 0.1f;
+        // plant index
+        _animator.SetFloat("PlantSwitch", plantIndex / 12f);
+        _animator.SetFloat("AnimSpeed", 0f);
     }
 
 
     void Update()
     {
+        /* 
+        _animator.speed = 0;
         // calculate all decays
         // get all inputs
         bool nutrientsInput = Input.GetKey(nutrientKey);
         bool growthInput = Input.GetKey(this.growthKey);
         bool reproduceInput = Input.GetKey(reproduceKey);
         // nutrient action feedback
-        nutrientFX.Run(nutrientsInput, _visual.sortingOrder);
-        reproduceFX.Run(reproduceInput, _visual.sortingOrder);
-        if (Input.GetKey(this.growthKey))
-        {
-            growth = Mathf.Clamp01(growth + Time.deltaTime * growthSpeed);
-        }
-        // growth action feedback
-        var scale = transform.localScale;
-        scale.y = growth;
-        transform.localScale = scale;
+        //nutrientFX.Run(nutrientsInput, _visual.sortingOrder);
+        */
+        //reproduceFX.Run(reproduceInput, _visual.sortingOrder);
+        activeGrowth = Input.GetKey(this.growthKey);
+        activeNutrient = Input.GetKey(this.nutrientKey);
+
+        // final number cleanup
+        _animator.SetFloat("Growth", growth);
+        _animator.SetFloat("Health", health);
     }
 
     public void RandomizeInputs()
