79e01f96655685726220a37524454658403b6991
Enric Llagostera <enricllagostera@gmail.com>
2018-03-14 15:20:37 -0400

    ground sprite randomizer

---
+++ well-prototype-v0_0_2/Assets/Roots/Ground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public Sprite[] allSprites;
+    private SpriteRenderer _sprite;
+
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _sprite.sprite = allSprites[Random.Range(0, allSprites.Length)];
+    }
+}
